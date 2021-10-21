0cada6b63afd6a3dd1912b6de1f044a0b457a9d2
Miguel Ángel <mangelc13@gmail.com>
2021-10-21 13:48:58 +0200

    forzar conflicto

---
+++ src/test/java/com/example/gitspringtest/MiguelTest.java
@@ -0,0 +1,11 @@
+package com.example.gitspringtest;
+
+import org.junit.jupiter.api.Test;
+
+public class MiguelTest {
+
+    @Test
+    void name() {
+        System.out.println("Holamundo");
+    }
+}
