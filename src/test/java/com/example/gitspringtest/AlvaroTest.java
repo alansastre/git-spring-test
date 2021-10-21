7ebd139f8b6c8aad8358bc70be3cbc11cef11c9c
alvarogarcia94 <91945932+alvarogarcia94@users.noreply.github.com>
2021-10-21 13:57:15 +0200

    Add files via upload

---
+++ src/test/java/com/example/gitspringtest/AlvaroTest.java
@@ -0,0 +1,17 @@
+package com.example.gitspringtest;
+
+import org.junit.jupiter.api.Test;
+
+public class AlvaroTest {
+
+    @Test
+    void saludo(){
+        System.out.println("Hola mundo, desde JUnit");
+    }
+
+    @Test
+    void despedida(){
+        System.out.println("Adios mundo");
+
+    }
+}
