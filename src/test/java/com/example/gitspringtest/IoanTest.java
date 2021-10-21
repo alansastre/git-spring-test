bdd9365528a165094e7c657cb74df9e9b0dc9d24
ioan <55089021+IOANSTIRBU@users.noreply.github.com>
2021-10-21 13:45:41 +0200

    commit Ioan

---
+++ src/test/java/com/example/gitspringtest/IoanTest.java
@@ -0,0 +1,11 @@
+package com.example.gitspringtest;
+
+import org.junit.jupiter.api.Test;
+
+public class IoanTest {
+
+    @Test
+    void test1(){
+        System.out.println("Hola desde git xd");
+    }
+}
