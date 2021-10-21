37e24e653b44f0d7ad35e082838479086b4657d5
alansastre <alan@alansastre.co>
2021-10-21 13:42:30 +0200

    Initial commit

---
+++ src/test/java/com/example/gitspringtest/AlanTest.java
@@ -0,0 +1,11 @@
+package com.example.gitspringtest;
+
+import org.junit.jupiter.api.Test;
+
+public class AlanTest {
+
+    @Test
+    void name() {
+        System.out.println("Hola desde junit");
+    }
+}
