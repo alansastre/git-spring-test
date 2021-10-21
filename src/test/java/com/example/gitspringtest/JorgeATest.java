cb906dce952c8b01d298ec9ba7dac94a6967e0f8
Jorge <jorgeaparicio79@gmail.com>
2021-10-21 13:59:41 +0200

    commit2 JorgeATest

--- src/test/java/com/example/gitspringtest/JorgeATest.java
+++ src/test/java/com/example/gitspringtest/JorgeATest.java
@@ -5,13 +5,18 @@ import org.junit.jupiter.api.Test;
 public class JorgeATest {
 
         @Test
-        void name() {
+        void saludo1() {
             System.out.println("¡Hola a tod@s!");
         }
 
         @Test
-        void name2() {
+        void saludo2() {
             System.out.println("¿Qué tal?");
+        }
+
+        @Test
+        void saludo3() {
+            System.out.println("¿Cómo lo lleváis?");
         }
 
 }
