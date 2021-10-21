c7c972f6a6eb4845520696c012c2b246f1a885ef
Ismael Gonzalez <ismamontiel@hotmail.com>
2021-10-21 13:49:21 +0200

    Prueba

---
+++ src/test/java/com/example/gitspringtest/IsmaelTest.java
@@ -0,0 +1,11 @@
+package com.example.gitspringtest;
+
+import org.junit.jupiter.api.Test;
+
+public class IsmaelTest {
+
+    @Test
+    void estoEsUnaPrueba(){
+        System.out.println("hola");
+    }
+}
